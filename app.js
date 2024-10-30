e091ea43c8df6b5e7b8a90e30e3e14e3402100d1
danielpresamarmolejo <dan.pres.mar@gmail.com>
2024-10-30 11:47:48 -0600

    comit second

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import helmet from "helmet"; // Importa helmet
 import userRoutes from "./src/routes/user.routes.js";
 import projectRoutes from "./src/routes/project.routes.js";
 
@@ -14,7 +15,20 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-//sirve para que express entienda formato JSON y pueda manejar peticiones POST
+// Configuración de Helmet para CSP
+app.use(helmet({
+    contentSecurityPolicy: {
+        directives: {
+            defaultSrc: ["'self'"], // Permitir recursos desde el mismo origen
+            imgSrc: ["'self'", "data:", "https:"], // Permitir imágenes desde el mismo origen y HTTPS
+            scriptSrc: ["'self'", "https://apis.google.com"], // Permitir scripts desde el mismo origen y Google APIs
+            styleSrc: ["'self'", "https://fonts.googleapis.com"], // Permitir estilos desde el mismo origen y Google Fonts
+            // Agrega otras directivas según sea necesario
+        },
+    },
+}));
+
+// Permitir que Express entienda formato JSON
 app.use(express.json());
 
 app.use((err, req, res, next) => {
@@ -24,4 +38,5 @@ app.use((err, req, res, next) => {
 
 app.use('/api/users', userRoutes);
 app.use('/api/projects', projectRoutes);
+
 export default app;
