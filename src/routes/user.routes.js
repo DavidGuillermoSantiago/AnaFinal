51378a646dfebbbe7fd8fbbb08e9c341386ae86a
david <davidguillermosantiago@gmail.com>
2024-11-06 01:09:13 -0600

    ruta para obtener usuario por id

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import { createUser } from '../controllers/user.controller.js';
 import { signin } from '../controllers/auth.controller.js';
+import { getUserById } from '../controllers/user.controller.js';
 
 const router = Router();
 
 // Ruta para crear un nuevo usuario
 router.post('/register', createUser);
-router.post('/signin', signin); // Corregido el método aquí
+router.post('/signin', signin);
+router.get('/:userId', getUserById);
 
 export default router;
